b88f73475fa92b546baa6ee13003fe886d74aec8
sakib-xrz <sakibxrz21@gmail.com>
2023-11-19 19:38:20 +0600

    cors added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const blogRouter = require("./routes/blogRoute");
 const categoryRouter = require("./routes/categoryRoute");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
